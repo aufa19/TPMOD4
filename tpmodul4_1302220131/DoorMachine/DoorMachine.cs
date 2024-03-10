4286aca941929a064a5da090bc40609f27692f12
aufa <abdillahaufa79@gmail.com>
2024-03-11 02:08:10 +0700

    “menambahkan implementasi dengan table-driven”

---
+++ tpmodul4_1302220131/DoorMachine/DoorMachine.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul4_1302220131
+{
+    internal class DoorMachine
+    {
+        public enum StateKeys { TERBUKA, TERTUTUP, TERKUNCI, QUIT };
+        public enum StateKunci { KUNCI, TIDAKKUNCI };
+
+        public void run()
+        {
+            StateKeys defaults = StateKeys.TERKUNCI;
+            String Command = "terbuka";
+            while (defaults != StateKeys.QUIT)
+            {
+                switch (defaults)
+                {
+                    case StateKeys.TERBUKA:
+                        Console.WriteLine("Pintu sudah terbuka");
+                        break;
+                    case StateKeys.TERKUNCI:
+                        Console.WriteLine("Pintu sudah terkunci");
+                        break;
+                    case StateKeys.QUIT:
+                        Console.WriteLine("Keluar");
+                        break;
+                }
+                Console.WriteLine("Masukkan perintah : ");
+                Command = Console.ReadLine();
+
+                if (Command == "BukaPintu" || Command == "buka")
+                {
+                    if (defaults == StateKeys.TERKUNCI)
+                    {
+                        defaults = StateKeys.TERBUKA;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Pintu tidak terkunci");
+                    }
+                }
+                else if (Command == "KunciPintu" || Command == "kunci")
+                {
+                    if (defaults == StateKeys.TERBUKA)
+                    {
+                        defaults = StateKeys.TERKUNCI;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Pintu terkunci");
+                    }
+                }
+                else if (Command == "quit" || Command == "QUIT")
+                {
+                    defaults = StateKeys.QUIT;
+                }
+                else
+                {
+                    Console.WriteLine("Perintah tidak dikenal");
+                }
+            }
+        }
+    }
+}
--- tpmodul4_1302220131/Program.cs
+++ tpmodul4_1302220131/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul4_1302220131;
+using static tpmodul4_1302220131.DoorMachine;
+using static tpmodul4_1302220131.KodePos;
+using System;
+
+namespace tpmodul4_1302220131
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Contoh pertama :");
+            KodePos kodepos = new KodePos();
+            kodepos.setKodePost();
+            kodepos.getKodePos();
+
+            Console.WriteLine("\nSoal ketiga :");
+            DoorMachine door = new DoorMachine();
+            door.run();
+        }
+    }
+}
